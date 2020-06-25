d6da64002ebdc926611b01109ed9c19f812c1843
Ruben <ruben.csepke@gmail.com>
2020-06-25 19:49:14 +0200

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVirus } from '@fortawesome/free-solid-svg-icons';
 import './sass/styles.scss';
 
 import Cards from './components/Cards/Cards.js';
 
+
 const App = () => {
   return (
     <div className="App">
+      <header id="header" className="centered">
+        <h1><span className="header-icon"><FontAwesomeIcon icon={faVirus} /></span> COVID-19 Tracker</h1>
+      </header>
       <main className="centered">
         <Cards />
       </main>
