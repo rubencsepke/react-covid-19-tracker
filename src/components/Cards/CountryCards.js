7bd19c5caab0f1402242c272b2b94b0c4bda33a4
Ruben <ruben.csepke@gmail.com>
2020-06-28 17:57:49 +0200

    Added search function

--- src/components/Cards/CountryCards.js
+++ src/components/Cards/CountryCards.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import CountryCard from './CountryCard.js';
 
@@ -7,8 +9,10 @@ class CountryCards extends Component {
     super(props);
     this.state = {
       isLoading: false,
-      countries: []
+      countries: [],
+      searchResults: []
     }
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   async getCountriesAsync() {
@@ -20,6 +24,16 @@ class CountryCards extends Component {
     this.setState({countries: countriesData, isLoading: false});
   }
 
+  handleSearch(e) {
+    e.preventDefault();
+    this.setState({isLoading: true});
+    let results = [];
+    results = this.state.countries.filter(data => {
+      return data.country.toLowerCase().includes(e.target.value.toLowerCase())
+    });
+    this.setState({searchResults: results, isLoading: false});
+  }
+
   componentDidMount() {
     this.getCountriesAsync();
   }
@@ -27,9 +41,20 @@ class CountryCards extends Component {
   render() {
     return (
       <section id="countries-wrapper" className="countries">
+        <div className="filter-wrap">
+          <div className="search-input">
+            <input type="text" placeholder="Search by country" onChange={this.handleSearch} />
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
+        </div>
         {this.state.isLoading ? 
           <div className="loading">Loading...</div> :
-          this.state.countries.map((data,index) => <CountryCard key={index} data={data} />)
+          <div className="country-list"> 
+            {this.state.searchResults.length > 0 ? 
+              this.state.searchResults.map((data,index) => <CountryCard key={index} data={data} />) :
+              this.state.countries.map((data,index) => <CountryCard key={index} data={data} />)
+            }
+          </div>
         }
       </section>
     )
