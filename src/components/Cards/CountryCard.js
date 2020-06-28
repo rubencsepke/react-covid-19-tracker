84376e55fc6f3f441f07abb7f4675953e55e624d
Ruben <ruben.csepke@gmail.com>
2020-06-28 18:54:01 +0200

    Updated CountryCard styles

--- src/components/Cards/CountryCard.js
+++ src/components/Cards/CountryCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookMedical, faSkullCrossbones, faHeart, faVial, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 const CountryCard = (props) => {
   return (
@@ -7,18 +9,22 @@ const CountryCard = (props) => {
       <div className="country-card-name">{props.data.country}</div>
       <ul className="country-card-info-list">
         <li className="country-card-info-item">
+          <FontAwesomeIcon className="icon" icon={faBookMedical} />
           <div className="info-name">Cases</div>
           <div className="info-value">{props.data.cases}</div>
         </li>
         <li className="country-card-info-item">
+          <FontAwesomeIcon className="icon" icon={faSkullCrossbones} />
           <div className="info-name">Deaths</div>
           <div className="info-value">{props.data.deaths}</div>
         </li>
         <li className="country-card-info-item">
+          <FontAwesomeIcon className="icon" icon={faHeart} />
           <div className="info-name">Recovered</div>
           <div className="info-value">{props.data.recovered}</div>
         </li>
         <li className="country-card-info-item">
+          <FontAwesomeIcon className="icon" icon={faVial} />
           <div className="info-name">Tests</div>
           <div className="info-value">{props.data.tests}</div>
         </li>
