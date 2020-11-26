6d37b65718c8af287361c00a707011918a483882
Ruben <ruben.csepke@gmail.com>
2020-11-26 19:58:27 +0100

    Added responsive

--- src/components/Table/TableBody.js
+++ src/components/Table/TableBody.js
@@ -8,24 +8,31 @@ const TableBody = (props) => {
           <strong>{props.data.country}</strong>
         </div>
         <div className="country-info">
+          <span className="country-info-title">Total Cases</span>
           <span className="country-info-value">{props.data.cases}</span>
         </div>
         <div className="country-info">
+          <span className="country-info-title">Today Cases</span>
           <span className="country-info-value red">{props.data.todayCases > 0 && `+`+props.data.todayCases}</span>
         </div>
         <div className="country-info">
+          <span className="country-info-title">Total Deaths</span>
           <span className="country-info-value">{props.data.deaths}</span>
         </div>
         <div className="country-info">
+          <span className="country-info-title">Today Deaths</span>
           <span className="country-info-value red">{props.data.todayDeaths > 0 && `+`+props.data.todayDeaths}</span>
         </div>
         <div className="country-info">
+          <span className="country-info-title">Total Recovered</span>
           <span className="country-info-value">{props.data.recovered}</span>
         </div>
         <div className="country-info">
+          <span className="country-info-title">Today Recovered</span>
           <span className="country-info-value green">{props.data.todayRecovered > 0 && `+`+props.data.todayRecovered}</span>
         </div>
         <div className="country-info">
+          <span className="country-info-title">Tests</span>
           <span className="country-info-value">{props.data.tests}</span>
         </div>
       </div>
